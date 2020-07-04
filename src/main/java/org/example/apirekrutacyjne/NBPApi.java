ceaf1bf69e18d6be3df49331bc2c35c05406f08d
Mateusz Grzeskowiak <mateusz.r.grzeskowiak@gmail.com>
2020-07-04 13:51:13 +0200

    Logger added

--- src/main/java/org/example/apirekrutacyjne/NBPApi.java
+++ src/main/java/org/example/apirekrutacyjne/NBPApi.java
@@ -1,11 +1,14 @@
 package org.example.apirekrutacyjne;
 
+import lombok.extern.log4j.Log4j;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
+@Log4j
 public class NBPApi {
     private final static String API_BID_ASK_ENDPOINT = "http://api.nbp.pl/api/exchangerates/rates/C/{currency}/{startDate}/{endDate}/?format-json";
 
@@ -15,7 +18,7 @@ public class NBPApi {
 
     public void requestBidAskRates(NBPApiParameters parameters) {
         String requestUrl = prepareRequestURL(parameters);
-        System.out.println("Request URL:" + requestUrl);
+        log.info("Request URL:" + requestUrl);
 
         HttpRequest request = HttpRequest.newBuilder()
                 .GET()
@@ -26,9 +29,9 @@ public class NBPApi {
             HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
 
             if (response.statusCode() == 200) {
-                System.out.println("Response: " + response.body());
+               log.info("Response: " + response.body());
             } else
-                System.err.println("Error: " + response.statusCode());
+                log.error("Error: " + response.statusCode());
 
         } catch (IOException e) {
             e.printStackTrace();
